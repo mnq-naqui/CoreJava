c25cbb0ff31fcf0b8d38177d9328aee863abc585
mnq-naqui <mnq.naqui@gmail.com>
2016-05-15 18:01:21 +0530

    Solution added

--- ProblemsOfInterview/src/mnq/naqui/InfyQue.java
+++ ProblemsOfInterview/src/mnq/naqui/InfyQue.java
@@ -6,17 +6,64 @@ package mnq.naqui;
 
 public class InfyQue {
 
+	
 	public static void main(String[] args) {
 	int [] array={1,0,1,1,0,1,1,0,0,1};
-	int [] b=null;
-	int j=0;
-		for (int i = 0; i < array.length; i++) {
-			if (array[i]==0) {
-				b[j]=array[i];
-				j++;
+	int countOfZeros=0;
+	
+	solution1(array, countOfZeros);
+		
+	solution2(array, countOfZeros);
 			}
+	
+	
+	//solution 1
+	private static void solution1(int[] array, int countOfZeros) {
+		for (int i = 0; i < array.length; i++) { 
+			
+			if (array[i]==0){
+				
+				countOfZeros++;
+				
+			}
+		}
+		for (int i = 0; i < array.length; i++) { 
+			
+			if (countOfZeros>i){
+				
+				array[i]=0;
+				
+			}else{
+				array[i]=1;
+			}
+		}
+		System.out.println(countOfZeros);
+		
+		for (int i : array) {
+			System.out.print(i+",");
 			
 		}
-		
+	}
+	
+	//solution 2
+		private static void solution2(int[] array, int countOfZeros) {
+			for (int i = 0; i < array.length; i++) { 
+				
+				if (array[i]==0){
+					
+					array[i]=0;
+					countOfZeros++;
+					
+				}else{
+					array[i]=1;
+				}
 			}
+			
+			for (int i : array) {
+				System.out.println(i);
+				
+			}
+		}
+		
+		
 }
